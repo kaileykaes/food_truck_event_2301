0fd0ae256c04aefc99494e9b5c25494aae8c8c76
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-03-08 09:30:51 -0700

    test: check_stock and stock methods

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -10,4 +10,32 @@ RSpec.describe FoodTruck do
     expect(@food_truck.name).to eq("Rocky Mountain Pies")
     expect(@food_truck.inventory).to eq({})
   end
+
+  it 'checks stock' do
+    expect(@food_truck.check_stock(@item1)).to eq(0)
+  end
+
+  xit 'can stock items' do
+    @food_truck.stock(@item1, 30)
+    expect(food_truck.inventory).to eq({@item1 => 30})
+    expect(food_truck.check_stock(@item1)).to eq(30)
+  end
+
+  xit 'can stock many items' do
+    @food_truck.stock(@item1, 30)
+    @food_truck.stock(@item1, 25)
+    expect(food_truck.inventory).to eq({@item1 => 55})
+    expect(food_truck.check_stock(@item1)).to eq(55)
+  end
+
+  xit 'can stock different items' do
+    @food_truck.stock(@item1, 30)
+    @food_truck.stock(@item1, 25)
+    @food_truck.stock(@item2, 12)
+    expect(food_truck.inventory).to eq({@item1 => 55, @item2 => 12})
+  end
+
+
+
+  
 end
