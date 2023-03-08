08067f1ae4d0e8098adb821f2a46921a0ec6af6f
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-03-08 10:14:01 -0700

    Feat: #food_truck_names

--- lib/event.rb
+++ lib/event.rb
@@ -8,6 +8,15 @@ class Event
   end
 
   def add_food_truck(food_truck)
-    @food_trucks << food_truck
+    food_trucks << food_truck
   end
+
+  def food_truck_names
+    names = []
+    food_trucks.each do |food_truck|
+      names << food_truck.name
+    end
+    names
+  end
+
 end
