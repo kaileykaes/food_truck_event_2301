1f3cd8a06849a22e14463e26f557ce18f5cb2bb2
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-03-08 10:48:27 -0700

    Feat: #potential revenue

--- lib/food_truck.rb
+++ lib/food_truck.rb
@@ -1,6 +1,6 @@
 class FoodTruck
-  attr_reader :name, 
-             :inventory
+  attr_reader :name 
+  attr_accessor :inventory
   
   def initialize(name)
     @name = name
@@ -25,4 +25,15 @@ class FoodTruck
   @inventory
   end
 
+  def potential_revenue
+    price_amount = {}
+    inventory.map do |item, amount|
+      price_amount[item.price] = amount
+    end
+    revenues = []
+    price_amount.map do |price, amount|
+      revenues << price * amount
+    end
+    revenues.sum
+  end
 end
