d174e594a63219e2dac86b603f045387d993f669
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-03-08 09:35:45 -0700

    refactor: fix expect statements

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -15,27 +15,23 @@ RSpec.describe FoodTruck do
     expect(@food_truck.check_stock(@item1)).to eq(0)
   end
 
-  xit 'can stock items' do
+  it 'can stock items' do
     @food_truck.stock(@item1, 30)
-    expect(food_truck.inventory).to eq({@item1 => 30})
-    expect(food_truck.check_stock(@item1)).to eq(30)
+    expect(@food_truck.inventory).to eq({@item1 => 30})
+    expect(@food_truck.check_stock(@item1)).to eq(30)
   end
 
-  xit 'can stock many items' do
+  it 'can stock many items' do
     @food_truck.stock(@item1, 30)
     @food_truck.stock(@item1, 25)
-    expect(food_truck.inventory).to eq({@item1 => 55})
-    expect(food_truck.check_stock(@item1)).to eq(55)
+    expect(@food_truck.inventory).to eq({@item1 => 55})
+    expect(@food_truck.check_stock(@item1)).to eq(55)
   end
 
   xit 'can stock different items' do
     @food_truck.stock(@item1, 30)
     @food_truck.stock(@item1, 25)
     @food_truck.stock(@item2, 12)
-    expect(food_truck.inventory).to eq({@item1 => 55, @item2 => 12})
+    expect(@food_truck.inventory).to eq({@item1 => 55, @item2 => 12})
   end
-
-
-
-  
 end
