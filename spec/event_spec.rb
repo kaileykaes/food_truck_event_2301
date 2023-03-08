7122506d652469a47294e1683dc15b9293632247
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-03-08 11:53:19 -0700

    refactor

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -49,7 +49,7 @@ RSpec.describe Event do
     expect(@event.food_truck_names).to eq(["Rocky Mountain Pies", "Ba-Nom-a-Nom", "Palisade Peach Shack"])
   end
 
-  it 'lists food truck names' do
+  it 'lists food trucks that sell' do
     @event.add_food_truck(@food_truck1)
     @event.add_food_truck(@food_truck2)
     @event.add_food_truck(@food_truck3)
