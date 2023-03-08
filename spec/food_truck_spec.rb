2a350cb037133ae72bb6453b6f3b253eae9d4203
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-03-08 09:48:20 -0700

    refactor: before(:each)

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -3,6 +3,14 @@ require 'spec_helper'
 RSpec.describe FoodTruck do
   before(:each) do
     @food_truck = FoodTruck.new("Rocky Mountain Pies")
+    @item1 = Item.new({
+      name: 'Peach Pie (Slice)', 
+      price: "$3.75"
+      })
+    @item2 = Item.new({
+      name: 'Apple Pie (Slice)', 
+      price: '$2.50'
+      })
   end
 
   it 'exists and has attributes' do
@@ -28,9 +36,10 @@ RSpec.describe FoodTruck do
     expect(@food_truck.check_stock(@item1)).to eq(55)
   end
 
-  xit 'can stock different items' do
+  it 'can stock different items' do
     @food_truck.stock(@item1, 30)
     @food_truck.stock(@item1, 25)
+    # require 'pry'; binding.pry
     @food_truck.stock(@item2, 12)
     expect(@food_truck.inventory).to eq({@item1 => 55, @item2 => 12})
   end
