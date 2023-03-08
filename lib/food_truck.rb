901c6fe919085abc61b345c0d51e45c7229c1b1c
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-03-08 09:33:22 -0700

    Feat: #check_stock

--- lib/food_truck.rb
+++ lib/food_truck.rb
@@ -1,3 +1,17 @@
 class FoodTruck
+  attr_reader :name, 
+              :inventory
   
+  def initialize(name)
+    @name = name
+    @inventory = {}
+  end
+
+  def check_stock(item)
+      if @inventory.include?(item)
+        return @inventory[item]
+      else
+        @inventory[item] = 0
+      end
+  end 
 end
